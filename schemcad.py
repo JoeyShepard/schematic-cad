4763fa4dd918ad597a3a5a5dc07f092947e515f4
JoeyShepard <joey.m.shepard@gmail.com>
2022-05-01 21:44:11 -0400

    second commit

---
+++ schemcad.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from sys import path
+from os.path import expanduser
+path.append(expanduser("~/python/"))
+
+#from color import *
+#from sys import exit
+
+print("Hello!")
